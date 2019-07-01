b881ea4a4b129a57c52cc9c3fb5758c0326c2e79
yico1 <yico1@DESKTOP-JQ0QAAV>
2019-07-01 17:18:30 +0300

    Add project files.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Untitled Page
+    </title>
+</head>
+<body>
+    <form id="form1" runat="server">  
+    <h3> TextBox Example </h3>  
+    <p>  
+        <asp:TextBox ID="Value1" text=" " runat="server"/>  
+
+        <asp:TextBox ID="Value2" text=" " runat="server"/>  
+
+
+        <asp:Label ID="AnswerMessage" runat="server"/>  
+    </p>  
+    <p>  
+        <asp:Button ID="AddButton" Text="Add" OnClick="AddButton_Click" runat="server"/>  
+    </p>  
+
+  </form>  
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void AddButton_Click(object sender, EventArgs e)
+        {
+            String ConnectionString = "Server=DESKTOP-JQ0QAAV;Database=twitter;Trusted_Connection=true";
+            SqlConnection conn = new SqlConnection(ConnectionString);
+
+            String mynewstr = "INSERT INTO mesajlar(isim, mesaj) VALUES(' "  + Value1.Text + "','" + Value2.Text + "')";
+
+            Response.Write(mynewstr);
+
+            SqlCommand command = new SqlCommand(mynewstr, conn);
+            conn.Open();
+
+            command.ExecuteNonQuery();
+
+            Response.Write("Başarıyla eklendi");
+            conn.Close();
+
+
+        }
+    }
+}
